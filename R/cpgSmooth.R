55ab7000333d2acd51e7ce5e17e3e18e9df81701
Tim Triche, Jr <ttriche@usc.edu>
2012-06-25 07:34:11 -0700

    simplify smoothing so it's easier to debug

--- R/cpgSmooth.R
+++ R/cpgSmooth.R
@@ -1,35 +1,33 @@
-cpgSmooth <- function (SE, assay = NULL, decay = 1000, impute = TRUE) {
+getWeight <- function(x, GR, decay=1000) {
+  raw = 1-log((distance(GR[queryHits(x)], GR[subjectHits(x)])+1), base=decay)
+  raw / sum(raw) # normalized
+}
+
+reWeightCpG <- function(x, tmp, idx) {
+  return(t(t(tmp[subjectHits(idx[[x]]), , drop = F]) %*% wts[[x]]))
+}
+
+cpgWeight <- function (SE, decay=1000) {
+  GR <- rowData(SE)
+  ol <- findOverlaps(resize(GR, 2*decay, fix="center"), GR)
+  idx <- split(ol, queryHits(ol))
+  wts <- lapply(idx, getWeight, GR=GR, decay=decay)
+  res <- list(wts=wts, idx=idx)
+  return(res)
+}
+
+cpgSmooth <- function (SE, w=NULL, decay=1000, assay=NULL, impute=T) {
   require(impute)
-  require(parallel)
-  options("mc.cores"=min(12, detectCores())) ## will thrash otherwise
-  GR <- rowData(SE)[ grep('^cg', rownames(SE)), ] 
+  GR <- rowData(SE)
   if(is.null(assay)) assay = names(assays(SE, withDimnames = F))[[1]]
-  stopifnot(unique(genome(rowData(SE))) == 'hg19')
-  message('Subsetting by arm...')
-  data(hg19.by.arm)
-  GR.by.arm <- lapply(hg19.by.arm, function(x) subsetByOverlaps(GR, x))
-  message(paste('Parallelizing by arm, over', options("mc.cores"), 'cores...'))
-  by.arm <- mclapply(GR.by.arm, function(GR.arm) {
-    print(paste('Locating neighbors on', unique(seqnames(GR.arm)), '...'))
-    w <- findOverlaps(resize(GR.arm, 2 * decay, fix = "center"), GR.arm)
-    idx <- split(w, queryHits(w))
-    print(paste('Computing weights for', unique(seqnames(GR.arm)), '...'))
-    getWeights <- function(x) { # {{{
-      raw <- 1 - log(base = decay,
-                     distance(GR.arm[queryHits(x)], GR.arm[subjectHits(x)]) + 1)
-      return(raw / sum(raw))
-    } # }}}
-    wts <- lapply(idx, getWeights)
-    arm.probes <- match(names(GR.arm), rownames(SE))
-    tmp <- assays(SE, F)[[assay]][arm.probes, ]
-    if(impute == TRUE && anyMissing(tmp)) tmp <- impute.knn(tmp)$data
-    print(paste('Smoothing', unique(seqnames(GR.arm), '...')))
-    reWeight <- function(x) { # {{{
-      t(t(tmp[subjectHits(idx[[x]]), , drop = F]) %*% wts[[x]])
-    } # }}}
-    smoothed <- do.call(rbind, lapply(as.numeric(names(idx)), reWeight))
-    rownames(smoothed) <- names(GR.arm)
-    return(smoothed)
-  })
-  return(by.arm)
+  if(length(unique(seqnames(rowData(SE)))) > 1) message('You CAN split by chr!')
+  if(is.null(w)) w <- cpgWeight(SE, decay=decay)
+  idx <- w$idx
+  wts <- w$wts
+  tmp <- assays(SE, F)[[assay]]
+  if(impute == TRUE && anyMissing(tmp)) tmp <- impute.knn(tmp)$data
+  print(paste('Smoothing', unique(seqnames(GR.arm), '...')))
+  smoothed <- do.call(rbind, lapply(names(idx), reWeightCpG, tmp=tmp, idx=idx))
+  rownames(smoothed) <- rownames(SE)
+  return(smoothed)
 }
