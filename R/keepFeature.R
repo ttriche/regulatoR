a5726f474a3017fe4b9bf0808fbc622facd9886d
Tim Triche, Jr <ttriche@usc.edu>
2012-06-28 14:49:50 -0700

    tweaks to make mixture filtering go faster

--- R/keepFeature.R
+++ R/keepFeature.R
@@ -1,4 +1,5 @@
 keepFeature <- function(x, minSep=0.2, G=c(1:5), ...) {
+  stopifnot(is(x, 'matrix'))
   require(parallel)
   unlist(mclapply(rownames(x), function(y) {
     return( mixFilter(x[y,],minSep=minSep,G=G)$retain > 1 )
--- R/mixFilter.R
+++ R/mixFilter.R
@@ -1,5 +1,4 @@
-mixFilter <-
-function(x, minSep=0.2, G=c(1:5), ...) {
+mixFilter <- function(x, minSep=0.2, G=c(2:5), ...) {
   bak <- list(values=x, retain=0)
   res <- try(clusterAndCollapse(x, minSep=minSep, G=G), silent=TRUE)
   if(!inherits(res, 'try-error')) {
