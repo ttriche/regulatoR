6095f20ceeca9afebfdf4f7783689979609f901a
Tim Triche, Jr <ttriche@usc.edu>
2012-07-03 18:43:11 -0700

    add normalization to getReadsPerSite

--- R/getReadsPerSite.R
+++ R/getReadsPerSite.R
@@ -1,16 +1,18 @@
 ## fetch reads, eventually normalized, from a BED or BAM file
-getReadsPerSite <- function(filename, SEorGR) {
+getReadsPerSite <- function(filename, SEorGR, normalize=TRUE) {
   if(is(SEorGR, 'SummarizedExperiment')) SEorGR <- rowData(SEorGR)
   if(grepl('(bed|gz)$', filename, ignore=T)) {
     ## it's a bed file
     require(rtracklayer)
     message('You could use bedops for this, and it would be much faster...')
-    bed.GR <- import(filename)
-    reads <- countOverlaps(SEorGR, bed.GR)
+    bed.GR <- import(filename, asRangedData=FALSE)
+    values(SEorGR)$counts <- countOverlaps(SEorGR, bed.GR)
   } else if(grepl('bam$', filename, ignore=T)) {
     ## it's a BAM file
-    message('BAM files can be real pigs, maybe convert it to a BED file first?')
-    stop('BAM support remains unfinished at the moment')  
+    stop('BAM support remains unfinished at the moment; try converting to BED.')
   }
-  return(reads)
+  if(normalize==TRUE) {
+    values(SEorGR)$counts <- values(SEorGR)$counts/length(bed.GR)
+  }
+  return(SEorGR)
 }
